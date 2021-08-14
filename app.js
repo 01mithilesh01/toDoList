9bec077644d91ea30e358b1739df9fc3fbb7596c
Mithilesh Patil <64639562+01mithilesh01@users.noreply.github.com>
2021-08-14 19:47:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 
 const mongoose = require("mongoose");
 
+const fetch = require("node-fetch");
+
 const app = express();
 
 // for using ejs and it will search list.ejs in views folder.
@@ -11,13 +13,21 @@ app.set("view engine", "ejs");
 
 
 // connecting mongoose to mongodb
-mongoose.connect("mongodb+srv://admin-mithilesh:YY3s9yzkfiUr7uC3@cluster0.054ls.mongodb.net/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://admin-mithilesh:HQhnqIq2mXB4TtN7@cluster0.054ls.mongodb.net/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true });
 
 const itemsSchema = { 
     name : String,
-    date : String
+    date : String,
+    ipAdd : String
 };
 const Item = mongoose.model("Item", itemsSchema);
+
+var ip;
+fetch('http://api.ipify.org/?format=json')
+    .then(results => results.json())
+    .then(data => ip=data.ip);
+
+console.log(ip);
 
 
 // Item.insertMany(defaultItems, function(err){
@@ -61,11 +71,12 @@ app.get("/", function(req, res){
 
 
     Item.find({}, function(err, foundItems){
-        res.render("toDoList", {listTitle : dateInFormat, newListItem:foundItems});
+        res.render("toDoList", {listTitle : dateInFormat, newListItem:foundItems, checkIP:ip});
     });
 
 
-    
+    console.log(ip, typeof(ip));
+    // console.log();
 
     // use to showcase the list.ejs page to the user. a dictionary is send to the list.ejs. So, the elements
     // of dictionary can be used in list.ejs
@@ -87,7 +98,8 @@ app.post("/", function(req, res){
     {
         const item = new Item({
             name : itemName,
-            date : dateInFormat
+            date : dateInFormat,
+            ipAdd : ip
         });
     item.save();
     }
